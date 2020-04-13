4f4fc3a159a96d0e781bf6dc367189924727edb3
Star CodeX <hello@bintangtobing.com>
2020-04-13 16:00:35 +0700

    update

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Movith - 1st Advertise Indoor TV</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon"
+        href="https://res.cloudinary.com/sarjanamalam/image/upload/v1586767431/movith/based/icon_stawjx.png"
+        alt="icon movith">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <style>
+        html {
+            width: 100%;
+            background: #d22d2d;
+        }
+
+        img {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            width: 300px;
+            height: auto;
+            transform: translate(-50%, -50%);
+            /* margin-top: -250px; */
+            /* Half the height */
+            /* margin-left: -250px; */
+            /* Half the width */
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <img src="https://res.cloudinary.com/sarjanamalam/image/upload/v1586768369/movith/based/letter-logodeveloping-white_tprlm3.png"
+        alt="Logo Movith">
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
