789ffd48a3d7b2e33d201be76b986c53f4a140bc
Star CodeX <hello@bintangtobing.com>
2020-04-15 14:00:16 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,56 +1,254 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title>Movith - 1st Advertise Indoor TV</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="INSPIRO" />
+    <meta name="description" content="1st Advertise Indoor TV">
     <link rel="shortcut icon"
         href="https://res.cloudinary.com/sarjanamalam/image/upload/v1586767431/movith/based/icon_stawjx.png"
         alt="icon movith">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <style>
-        html {
-            width: 100%;
-            background: #d22d2d;
-        }
-
-        img {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            width: 300px;
-            height: auto;
-            transform: translate(-50%, -50%);
-            /* margin-top: -250px; */
-            /* Half the height */
-            /* margin-left: -250px; */
-            /* Half the width */
-        }
-
-    </style>
+    <title>Movith - 1st Advertise Indoor TV</title>
+    <link href="css/plugins.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
 </head>
 
 <body>
+    <div class="body-inner">
+        <header id="header" data-fullwidth="true" class="header-mini dark header-always-fixed">
+            <div class="header-inner">
+                <div class="container">
 
-    <img src="https://res.cloudinary.com/sarjanamalam/image/upload/v1586768369/movith/based/letter-logodeveloping-white_tprlm3.png"
-        alt="Logo Movith">
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
+                    <div id="logo">
+                        <a href="/">
+                            <span class="logo-default"><img
+                                    src="https://res.cloudinary.com/sarjanamalam/image/upload/v1586767642/movith/based/letter-logo_hohje6.png"
+                                    alt=""></span>
+                            <span class="logo-dark"><img
+                                    src="https://res.cloudinary.com/sarjanamalam/image/upload/v1586767642/movith/based/letter-logo_hohje6.png"
+                                    alt=""></span>
+                        </a>
+                    </div>
+
+                    <div id="search"><a id="btn-search-close" class="btn-search-close" aria-label="Close search form"><i
+                                class="icon-x"></i></a>
+                        <form class="search-form" action="search-results-page.html" method="get">
+                            <input class="form-control" name="q" type="text" placeholder="Type & Search..." />
+                            <span class="text-muted">Start typing & press "Enter" or "ESC" to close</span>
+                        </form>
+                    </div>
+
+                    <div class="header-extras">
+                        <ul>
+                            <li>
+                                <a class="btn btn-danger" href="#"><i class="icon-user"></i> SIGN IN</a>
+                            </li>
+                            {{-- <li>
+                                <div class="p-dropdown">
+                                    <a href="#"><i class="icon-globe"></i><span>EN</span></a>
+                                    <ul class="p-dropdown-content">
+                                        <li><a href="#">French</a></li>
+                                        <li><a href="#">Spanish</a></li>
+                                        <li><a href="#">English</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li>
+                                <a href="#"><i class="icon-user"></i></a>
+                            </li> --}}
+                        </ul>
+                    </div>
+
+                    <div id="mainMenu-trigger">
+                        <a class="lines-button x"><span class="lines"></span></a>
+                    </div>
+
+                    <div id="mainMenu" class="menu-left menu-lines">
+                        <div class="container">
+                            <nav>
+                                <ul>
+                                    <li><a href="/">Home</a></li>
+                                    <li><a href="#">Ads</a></li>
+                                    <li><a href="#">Box Office</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </header>
+    </div>
+    <div id="slider" class="inspiro-slider slider-halfscreen dots-creative" data-height-xs="360" data-autoplay="2600"
+        data-animate-in="fadeIn" data-animate-out="fadeOut" data-items="1" data-loop="true" data-autoplay="true">
+
+        <div class="slide background-image"
+            style="background-image:url('https://res.cloudinary.com/sarjanamalam/image/upload/v1586850034/movith/example/poster-1_ulszbm.jpg');">
+            {{-- <div class="bg-overlay"></div> --}}
+        </div>
+        <div class="slide background-image"
+            style="background-image:url('https://res.cloudinary.com/sarjanamalam/image/upload/v1586850034/movith/example/poster-2_x7ma9s.jpg');">
+            {{-- <div class="bg-overlay"></div> --}}
+        </div>
+        <div class="slide background-image"
+            style="background-image:url('https://res.cloudinary.com/sarjanamalam/image/upload/v1586850034/movith/example/poster-3_oumvtj.jpg');">
+            {{-- <div class="bg-overlay"></div> --}}
+        </div>
+    </div>
+    <section class="section-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 m-t-100">
+                    <h1>1<sup>st</sup> Advertise Indoor TV.
+                    </h1>
+                    <h3>Reach out and watch your project on our client's Smart TV and get more offers on advertising on
+                        our indoor TV.</h3>
+                </div>
+                <div class="col-lg-4 text-right">
+                    <img src="https://res.cloudinary.com/sarjanamalam/image/upload/v1586857376/movith/example/Apple_TV_Mockup_oqpnxi.png"
+                        alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Ready to get advertise?</h1>
+                    <div class="input-group">
+                        <input type="email" class="form-control" placeholder="Email address">
+                        <div class="input-group-append">
+                            <span class="input-group-text">Try it now</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer id="footer" class="inverted">
+        <div class="footer-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-2 col-lg-2 col-md-3">
+
+                        <div class="widget">
+                            <h4>PROJECT</h4>
+                            <ul class="list">
+                                <li><a href="#">Latest Release</a></li>
+                                <li><a href="#">Updates</a></li>
+                                <li><a href="#">License</a></li>
+                                <li><a href="#">News</a></li>
+                                <li><a href="#">Links</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <div class="col-xl-2 col-lg-2 col-md-3">
+
+                        <div class="widget">
+                            <h4>SUPPORT</h4>
+                            <ul class="list">
+                                <li><a href="#">Troubleshooting</a></li>
+                                <li><a href="#">Common Questions</a></li>
+                                <li><a href="#">Report a Bug</a></li>
+                                <li><a href="#">Get Help</a></li>
+                                <li><a href="#">FAQS</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <div class="col-xl-2 col-lg-2 col-md-3">
+
+                        <div class="widget">
+                            <h4>COMPANY</h4>
+                            <ul class="list">
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Contact</a></li>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Portfolio</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <div class="col-xl-2 col-lg-2 col-md-3">
+
+                        <div class="widget">
+                            <h4>INFO</h4>
+                            <ul class="list">
+                                <li><a href="#">Get Directions</a></li>
+                                <li><a href="#">Call Us</a></li>
+                                <li><a href="#">Our Staff</a></li>
+                                <li><a href="#">Working Hours</a></li>
+                                <li><a href="#">Offices</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-12">
+
+                        <div class="widget clearfix widget-newsletter">
+                            <h4 class="widget-title"><i class="fa fa-envelope"></i> Sign Up For a Newsletter</h4>
+                            <p>Weekly breaking news, analysis and cutting edge advices on job searching.
+                            </p>
+                            <form class="widget-subscribe-form p-r-40" action="include/subscribe-form.php" role="form"
+                                method="post" novalidate="novalidate">
+                                <div class="input-group">
+                                    <input aria-required="true" name="widget-subscribe-form-email"
+                                        class="form-control required email" placeholder="Enter your Email" type="email">
+                                    <span class="input-group-btn">
+                                        <button type="submit" id="widget-subscribe-submit-button" class="btn"><i
+                                                class="fa fa-paper-plane"></i></button>
+                                    </span> </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+
+                        <div class="social-icons social-icons">
+                            <ul>
+                                <li class="social-rss"><a href="#"><i class="fa fa-rss"></i></a></li>
+                                <li class="social-facebook"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li class="social-twitter"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li class="social-vimeo"><a href="#"><i class="fab fa-vimeo"></i></a></li>
+                                <li class="social-youtube"><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                <li class="social-pinterest"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                                <li class="social-gplus"><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-6 text-right">
+                        <div class="copyright-text">© 2019 POLO HTML5 Template. All Rights Reserved. </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="js/jquery.js"></script>
+    <script src="js/plugins.js"></script>
+
+    <script src="js/functions.js"></script>
 </body>
 
 </html>
